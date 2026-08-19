a73b3397a9f83c2bcbbadf15b69f4ba804c09a35
SAURABH YADAV <saurabhyadav@SAURABHs-MacBook-Air.local>
2026-08-19 16:35:21 +0530

    C++(10/08/2026)

---
+++ assign2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void f1()
+{
+   cout<<"hello mysirg";
+}
+void f2()
+{
+    cout<<"hello"<<endl<<"mysirg";
+}
+void f3()
+{
+    int a,b,c;
+    cout<<"enter two number:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum of "<<c;
+
+}
+void f4()
+{
+    int r;
+    float a;
+    cout<<"enter a radius:";
+    cin>>r;
+    a=3.14*r*r;
+    cout<<"area of circle:"<<a;
+}
+void f5()
+{
+    int l,b,h,v;
+    cout<<"enter length,breath and hieght:";
+    cin>>l>>b>>h;
+    v=l*b*h;
+    cout<<"volume of cubuoid"<<v;
+
+}
+
+
